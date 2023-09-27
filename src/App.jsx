72be12d0f543a49ee9f1027469584db7cad7392f
OwenRD <owenruizdiaz2004@gmail.com>
2023-09-27 20:27:13 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import './css/style-sidebar.css'
+import Sidebar from './components/Sidebar.jsx'
+import Footer from './components/Footer.jsx'
+import Body from './components/Body.jsx';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Sidebar/>
+      <Body/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+     <>
+
+      <div className='conteiner-bar'>
+
+      <nav className='elements-bar'>
+        <a href="#">Inicio</a>
+        <a href="#">Sombre mi</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Contacto</a>
+      </nav>
+      
+      </div>
+
+     </>
+  )
+}
+
+export default Sidebar
+
